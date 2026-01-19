f91118037ce106bb15496c92db3bdce5da744fd0
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-19 11:19:48 -0300

    Primeiro commit

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,12 @@
+import logo from "../../assets/logo.svg";
+
+export const Aside = () => {
+  return (
+    <aside className="bg-neutral-header max-w-70.5 min-h-screen py-20 flex flex-col gap-14">
+      <img src={logo} alt="" />
+      <p className="text-neutral-text text-[13px] leading-[120%] text-center">
+        Desenvolvido por Alura. Projeto sem fins comerciais.
+      </p>
+    </aside>
+  );
+};
