5b6431e364a5b63abf3eaa8d0d111e6fc74933ad
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-20 09:55:07 -0300

    Adiciona e estiliza cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
 import { Aside } from "./components/Aside"
+import Card, { CardHeader } from "./components/Card"
+import { Container } from "./components/Container"
+import { DailyBudget } from "./components/DailyBudget"
+import { Main } from "./components/Main"
+import { SavingsStatus } from "./components/SavingsStatus"
+import { SearchInput } from "./components/Searchinput"
+import { Typography } from "./components/Typography"
 
 function App() {
 
   return (
     <div className="bg-neutral-background min-h-screen">
-      <Aside />
+
+      <Container>
+        <Aside />
+        <Main>
+          <SearchInput name="q" />
+        <div>
+          <Typography variant="h1">
+            Olá, Vinny!
+          </Typography>
+          <Typography>
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
+        <section>
+          <Card>
+            <Card.Header>
+              Orçamento diário disponível:
+            </Card.Header>
+            <Card.Body>
+              <DailyBudget value={42} />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>
+              Progresso da meta financeira
+            </Card.Header>
+            <Card.Body>
+              <SavingsStatus />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>
+              Movimentação financeira
+            </Card.Header>
+            <Card.Body>
+              R$200
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>
+              Minhas contas
+            </Card.Header>
+            <Card.Body>
+              R$200
+            </Card.Body>
+          </Card>
+        </section>
+        </Main>
+      </Container>
+
     </div>
   )
 }
