1bf8b49f1fb2862e49fb059a5ba0c5fe94d8042f
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-20 10:43:18 -0300

    estiliza os dois cards do topo

--- src/components/SavingsStatus/index.jsx
+++ src/components/SavingsStatus/index.jsx
@@ -1,11 +1,13 @@
 import { IconSavings } from "../Icons"
+import { Progressbar } from "../ProgressBar"
 
 export const SavingsStatus = () => {
     return (
-        <div>
+        <div className="flex flex-col gap-10">
             <p className="text-secondary-income flex items-center gap-2 justify-center text-xl leading-[125%]">
                 <IconSavings /> Economizar
             </p>
+            <Progressbar percent={57}/>
         </div>
     )
 }
